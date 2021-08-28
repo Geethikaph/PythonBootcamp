f6d82a39a12af872b1e6c1fb1fd34cf7a6d6085a
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-28 22:01:17 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -4,3 +4,7 @@ print("Sliced list is : ", fthree)
 list_num[0] = list_num[4]=fthree[0] = fthree[2]= 0
 print("replaced list-1 : ",list_num)
 print("replaced list-2 : ",fthree)
+
+#challenge
+reverse_list = list_num[::-1]
+print("reverse list", reverse_list)
