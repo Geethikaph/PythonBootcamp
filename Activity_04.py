fec718d9265808be08b4415e3171846c8961f8be
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-28 20:56:50 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -4,11 +4,10 @@ sum = var1 + var2
 #1
 print("%d + %d = %d" %(var1, var2, sum))
 #2
-print('Sum of {var1} + {var2} is {sum}')
+print(f'Sum of {var1} + {var2} is {sum}')
 #3
 print (var1, '+', var2, '=',var1+var2)
 #4
 print("The sum of {0} and {1} is {2}".format(var1, var2, sum))
 #5
 print('The sum is %d' %(int(input('Enter first number: ')) + int(input('Enter second number: '))))
-
