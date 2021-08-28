69d9dc07b031e3fff1bb1f8871da4479f6f10bf2
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-28 21:20:53 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -1,6 +1,10 @@
-var = input("Enter five numbers : ")
-numbers  = var.split()
-sum = 0
-for num in numbers:
-    sum += int (num)
-print("Sum of all the numbers is = ", sum)
+list = []
+ 
+list = [int(item) for item in input("Enter five numbers : ").split()]
+list_sum = sum(list)
+
+print ("sum of all the numbers is", list_sum)
+
+
+
+
