2219f73cb9ff9e64640850346f7fb0df283e7c56
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-28 14:58:59 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,4 +1,6 @@
-var1 =  " good "
-var2 =  " morning "
+var1 =  input ("enter the first string")
+print(var1)
+var2 = input ("enter the second string ")
+print(var2)
 result = var1 + var2 + "\n"
 print (result*5) 
