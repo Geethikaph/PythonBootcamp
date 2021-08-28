be291c9cc98e85f580f6faff4b094cba1f386bd3
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-28 20:03:00 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,4 +1,13 @@
 var1 = int(input("Enter the first number: "))
 var2 = int(input("Enter the second number: "))
 sum = var1 + var2
+#1
 print("%d + %d = %d" %(var1, var2, sum))
+#2
+print(f'Sum of {var1} + {var2} is {sum}')
+#3
+print (var1, '+', var2, '=',var1+var2)
+#4
+print("The sum of {0} and {1} is {2}".format(var1, var2, sum))
+#5
+print('The sum is %d' %(int(input('Enter first number: ')) + int(input('Enter second number: '))))
