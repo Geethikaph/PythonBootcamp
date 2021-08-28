8fb4e08aa2a1d13fac43a28d8b20a0dc044e5d28
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-28 21:55:33 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -1,7 +1,6 @@
 list_num = [int(num) for num in input("Enter five numbers : ").split()]
 fthree = list_num[:3]
 print("Sliced list is : ", fthree)
-list_num[0] = list_num[4]= 0
+list_num[0] = list_num[4]=fthree[0] = fthree[2]= 0
 print("replaced list-1 : ",list_num)
-fthree[0] = fthree[2] = 0
 print("replaced list-2 : ",fthree)
