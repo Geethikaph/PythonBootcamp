dbf576f38c6d83ea26400c36a0d2b5b5c3e23181
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-28 19:22:06 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -2,5 +2,5 @@ var1 =  input ("enter the first string: ")
 print(var1)
 var2 = input ("enter the second string:")
 print(var2)
-result = var1 + var2 + " "
+result = var1 + " " + var2 + " "
 print (result + "\n" + result*5 )
