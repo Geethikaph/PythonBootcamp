49cb79365262bff202d4617d66294081976b1962
Geethikaph <89664461+Geethikaph@users.noreply.github.com>
2021-08-29 19:41:46 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -1,3 +1,4 @@
+import math
 l = float(input("Enter length: "))
 b = float(input("Enter breadth: "))
 h = float(input("Enter height: "))
@@ -6,6 +7,6 @@ k= (l*l) + (b*b) + (h*h)
 print(k)
 vol= ((b*b)*(h*h))/k**(1/2)
 print('Volume of tromboloid= ',format(vol,'0.3f'))
-rad=((3*vol)/(4*(22/7)))**(1/3)
+rad=((3*vol)/(4*math.pi))**(1/3)
 print('Radius of sphere= ',format(rad,'0.3f'))
 
